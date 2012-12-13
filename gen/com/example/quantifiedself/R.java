95eb1e4eb55b875ef931a3cc964a26548806a042
Michael Reeder <reeder@knoesis.org>
2012-12-13 04:39:32 -0500

    asdf

--- gen/com/example/quantifiedself/R.java
+++ gen/com/example/quantifiedself/R.java
@@ -20,20 +20,22 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int addDatabn=0x7f080008;
-        public static final int description=0x7f080002;
-        public static final int descriptionArea=0x7f080003;
-        public static final int fileUpload=0x7f080004;
-        public static final int menu_settings=0x7f080009;
-        public static final int submitbn=0x7f080007;
-        public static final int tagArea=0x7f080006;
-        public static final int tags=0x7f080005;
-        public static final int title=0x7f080000;
-        public static final int titleArea=0x7f080001;
+        public static final int addDatabn=0x7f080001;
+        public static final int description=0x7f080005;
+        public static final int linearLayout1=0x7f080002;
+        public static final int linearLayout2=0x7f080004;
+        public static final int linearLayout3=0x7f080006;
+        public static final int menu_settings=0x7f08000a;
+        public static final int myGrid=0x7f080000;
+        public static final int spinner=0x7f080008;
+        public static final int submit=0x7f080009;
+        public static final int tags=0x7f080007;
+        public static final int title=0x7f080003;
     }
     public static final class layout {
         public static final int activity_add_data=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int list_view_layout=0x7f030002;
     }
     public static final class menu {
         public static final int activity_add_data=0x7f070000;
